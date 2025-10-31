d36e008834e4df47f17086a30694972d457b0361
Keven L. Garcia S. <kevengarcyya@gmail.com>
2025-10-31 12:59:52 -0300

    31/10

--- Bd.py
+++ Bd.py
@@ -1,32 +1,37 @@
-agenda = {
-    #dias
-    "segunda": [
-        #horarios
+agenda = [
+    #Segunda
+    {
         {"id": "01", "horário": "08:00/09:00", "status": "livre", "cliente": None}, #blocos
         {"id": "02", "horário": "15:00/16:00", "status": "livre", "cliente": None},
-    ],
-    # "terça": [
-    #     {"horário": "08:00/09:00", "status": "livre"},
-    #     {"horário": "15:00/16:00", "status": "ocupado"}
-    # ],
-    # "quarta": [
-    #     {"horário": "08:00/09:00", "status": "livre"},
-    #     {"horário": "15:00/16:00", "status": "ocupado"}
-    # ],
-    # "quinta": [
-    #     {"horário": "08:00/09:00", "status": "livre"},
-    #     {"horário": "15:00/16:00", "status": "ocupado"}
-    # ],
-    # "sexta": [
-    #     {"horário": "08:00/09:00", "status": "livre"},
-    #     {"horário": "15:00/16:00", "status": "ocupado"}
-    # ],
-    # "sabado": [
-    #     {"horário": "08:00/09:00", "status": "livre"},
-    #     {"horário": "15:00/16:00", "status": "ocupado"}
-    # ],
-    # "domindo": [
-    #     {"horário": "08:00/09:00", "status": "livre"},
-    #     {"horário": "15:00/16:00", "status": "ocupado"}
-    # ]
-}
+    },
+    #Terça
+    {
+        {"id": "01", "horário": "08:00/09:00", "status": "livre", "cliente": None}, #blocos
+        {"id": "02", "horário": "15:00/16:00", "status": "livre", "cliente": None},
+    },
+    #Quarta
+    {
+        {"id": "01", "horário": "08:00/09:00", "status": "livre", "cliente": None}, #blocos
+        {"id": "02", "horário": "15:00/16:00", "status": "livre", "cliente": None},
+    },
+    #Quinta
+    {
+        {"id": "01", "horário": "08:00/09:00", "status": "livre", "cliente": None}, #blocos
+        {"id": "02", "horário": "15:00/16:00", "status": "livre", "cliente": None},
+    },
+    #Sexta
+    {
+        {"id": "01", "horário": "08:00/09:00", "status": "livre", "cliente": None}, #blocos
+        {"id": "02", "horário": "15:00/16:00", "status": "livre", "cliente": None},
+    },
+    #Sabado
+    {
+        {"id": "01", "horário": "08:00/09:00", "status": "livre", "cliente": None}, #blocos
+        {"id": "02", "horário": "15:00/16:00", "status": "livre", "cliente": None},
+    },
+    #Domingo
+    {
+        {"id": "01", "horário": "08:00/09:00", "status": "livre", "cliente": None}, #blocos
+        {"id": "02", "horário": "15:00/16:00", "status": "livre", "cliente": None},
+    },
+]
